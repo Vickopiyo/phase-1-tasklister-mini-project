6a64e7c5b388292c9a632faa26e7752528e76d42
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-14 09:07:02 +0300

    Adds functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.querySelector("DOMContentLoad",()=>{
+  const text =document.querySelector("input");
+  const form = document.querySelector("form");
+  form.addEventListener("submit",(e)=>{
+e.preventDefault();
+addToDo(e.target["new-task-description"].value)
+form.reset()
+  })
+})
+// your code here
+function addToDo(){
+  let li = document.createElement("li")
+  let btn=document.createElement('button')
+  btn.textContent="play ball"
+  btn.addEventListener('click',deleteList)
+  li.textContent= `${input}`
+  document.querySelector("#list").appendChild(li)
+  li.appendChild(btn)
+}
+function deleteList(e){ e.target.parentNode.remove()}
