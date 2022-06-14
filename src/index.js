cf333cb4d571a5c10f6ab9bf9eb4de0e2a0c021e
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-14 09:17:31 +0300

    Fully functional

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-document.querySelector("DOMContentLoad",()=>{
-  const text =document.querySelector("input");
-  const form = document.querySelector("form");
-  form.addEventListener("submit",(e)=>{
-e.preventDefault();
-addToDo(e.target["new-task-description"].value)
-form.reset()
-  })
-})
-// your code here
-function addToDo(){
-  let li = document.createElement("li")
-  let btn=document.createElement('button')
-  btn.textContent="play ball"
-  btn.addEventListener('click',deleteList)
-  li.textContent= `${input}`
-  document.querySelector("#list").appendChild(li)
-  li.appendChild(btn)
-}
-function deleteList(e){ e.target.parentNode.remove()}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	let form = document.querySelector('form')
+	form.addEventListener('submit', (e) =>{
+	  e.preventDefault()
+	  add(e.target['new-task-description'].value)
+	  form.reset()
+	})
+  });
+  
+  function add(input){
+	let li = document.createElement('li')
+	let btn = document.createElement('button')
+	btn.textContent = 'x'
+	btn.addEventListener('click', deleteList)
+	li.textContent = `${input}  `
+	document.querySelector('#list').appendChild(li)
+	li.appendChild(btn)
+	
+  }
+  
+  function deleteList(e){
+	e.target.parentNode.remove()
+  }
